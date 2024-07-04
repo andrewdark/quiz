c309bb5e8840ab8ae3a5c75a9cb509de4c5aac57
andrewdark <samsonov.a@ukr.net>
2024-07-04 10:56:17 +0300

    add button -ui element

---
+++ src/main/frontend/src/UI/Button/Button.tsx
@@ -0,0 +1,25 @@
+import classes from "./Button.module.scss";
+import React, {MouseEventHandler} from "react";
+
+interface ButtonProps {
+    type: string,
+    disabled: boolean,
+    onClick: MouseEventHandler<HTMLButtonElement>,
+    children?: React.ReactChild | React.ReactNode;
+}
+
+const Button = (props: ButtonProps) => {
+
+    const cls = [classes.Button, classes[props.type]];
+    return (
+        <button
+            className={cls.join(" ")}
+            onClick={props.onClick}
+            disabled={props.disabled}
+        >
+            {props.children}
+        </button>
+    );
+};
+
+export default Button;
