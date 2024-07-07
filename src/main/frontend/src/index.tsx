bddb8ae556c21a601c351d508850ec0eb84671bc
andrewdark <samsonov.a@ukr.net>
2024-07-07 16:22:29 +0300

    init new branch route

--- src/main/frontend/src/index.tsx
+++ src/main/frontend/src/index.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import './index.css';
 import App from './App';
 import { createRoot } from "react-dom/client";
+import {BrowserRouter} from "react-router-dom";
+
 const container = document.getElementById("root");
+const app = (<BrowserRouter><App /></BrowserRouter>);
+
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(app);
