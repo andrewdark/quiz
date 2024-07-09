33e62e14c9024d0b4ff22d93f24c1f2a04b18d5a
andrewdark <samsonov.a@ukr.net>
2024-07-09 11:39:40 +0300

    add auth form and style

--- src/main/frontend/src/containers/Auth/Auth.tsx
+++ src/main/frontend/src/containers/Auth/Auth.tsx
@@ -1,5 +1,6 @@
 import {Component} from "react";
 import classes from './Auth.module.scss';
+import Button from "../../UI/Button/Button";
 
 interface AuthProps {
 
@@ -11,8 +12,22 @@ interface AuthState {
 
 class Auth extends Component<AuthProps, AuthState> {
 
+    loginHandler = ():void =>{};
+    regHandler = ():void =>{};
+    submitHandler = ():void =>{};
+
     render() {
-        return <div className={classes.Auth}>AUTH</div>;
+        return <div className={classes.Auth}>
+            <div>
+                <h1>AUTHENTICATION</h1>
+                <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                    <input type="text"/>
+                    <input type="text"/>
+                    <Button type="success" onClick={this.loginHandler}>Login</Button>
+                    <Button type="primary" onClick={this.regHandler}>Registration</Button>
+                </form>
+            </div>
+        </div>;
 
     }
 }
